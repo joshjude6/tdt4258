8968ce7190045dbcff10661d1ca3ace1c64d1ed8
Josh Jude <joshjude6@gmail.com>
2025-08-29 18:41:37 +0200

    more comments!

--- lab1/palinfinder.s
+++ lab1/palinfinder.s
@@ -8,18 +8,18 @@ _start:
     mov r3, #0              // i = 0
     sub r4, r1, #1          // j = len-1
 
-    bl  check_palindrome
-    b   _exit
+    bl check_palindrome
+    b _exit
 	
 check_length:
-    ldrb r2, [r0, r1]
-    cmp  r2, #0
-    beq  end_cnt_length
-    add  r1, r1, #1
-    b    check_length
+    ldrb r2, [r0, r1] // loading byte at r0+r1 into r2
+    cmp  r2, #0 // comparing r2 to nullbyte
+    beq  end_cnt_length // if null, end the loop
+    add  r1, r1, #1 // increment r1
+    b    check_length // start over
 	
 end_cnt_length:
-    bx   lr 
+    bx lr // return from the function
 
 check_palindrome:
     // saving the r4 value since it is callee-saved
@@ -68,10 +68,10 @@ converting_r6: // same as right before with r5
 
 comp:
     cmp r5, r6
-    bne not_palindrome
+    bne not_palindrome // if r5 and r6 are not equal, the string is not a palindrome
     b   advance
 
-advance:
+advance: // increment one side and decrement the other
     add r3, r3, #1          // i++
     sub r4, r4, #1          // j--
     b   check_palindrome
@@ -85,34 +85,30 @@ dec:
     b   check_palindrome
 
 wild_left:
-    cmp r6, #32 // only checking if the corresponding character on the other side is a space, since it is a wildcard
-    beq not_palindrome // moving on if not space
-    add r3, r3, #1
-	sub r4, r4, #1
-    b   check_palindrome
+    cmp r6, #32 // wildcard can't match space
+    beq not_palindrome // moving on if r6 is not a space
+    b advance 
 
 wild_right:
-    cmp r5, #32 
-    beq not_palindrome 
-	add r3, r3, #1
-    sub r4, r4, #1
-    b   check_palindrome
+    cmp r5, #32 // wildcard can't match space
+    beq not_palindrome // moving on if r5 is not a space
+	b advance
 
 is_palindrome:
     pop {r4} // done to restore r4 and avoid clobbering
-    ldr r1, =0xFF200000
-	mov r0, #0x1F
-	str r0, [r1]
-	ldr r0, =is_palin
-    b write_jtag
+    ldr r1, =0xFF200000 // LED address
+	mov r0, #0x1F // value corresponds to LED 6-10
+	str r0, [r1] // activate LED
+	ldr r0, =is_palin // load correct string for is_palindrome
+    b write_jtag // move on to writing to JTAG
 
 not_palindrome:
     pop {r4} // done to restore r4 and avoid clobbering
     ldr r1, =0xFF200000
-	mov r0, #0x3E0
-	str r0, [r1]
-	ldr r0, =not_palin
-    b write_jtag
+	mov r0, #0x3E0 // value corresponds to LED 1-5
+	str r0, [r1] // activate LED
+	ldr r0, =not_palin // load correct string for not_palindrome
+    b write_jtag // move on to writing to JTAG
 	
 write_jtag:
 	ldr r1, =0xFF201000
@@ -126,6 +122,8 @@ _exit:
     b .
 
 .data
+.align
     input: .asciz "Grav ned den varg"
     not_palin: .asciz "Not a palindrome"
     is_palin: .asciz "Is a palindrome"
+.end
