0a01863028e3b3b22eca9b717e90c244c1ca8c37
Josh Jude <joshjude6@gmail.com>
2025-08-29 18:29:45 +0200

    added comments

--- lab1/palinfinder.s
+++ lab1/palinfinder.s
@@ -1,10 +1,10 @@
 .global _start
-
-
 _start:
     ldr r0, =input
 	mov r1, #0
     bl  check_length        // r1 = length of string
+
+    // initializing indexes here
     mov r3, #0              // i = 0
     sub r4, r1, #1          // j = len-1
 
@@ -22,7 +22,8 @@ end_cnt_length:
     bx   lr 
 
 check_palindrome:
-    push {r4}                // preserve callee-saved r4
+    // saving the r4 value since it is callee-saved
+    push {r4}
 
     // checking if i is greater or equal to j
     cmp r3, r4
@@ -51,13 +52,13 @@ check_palindrome:
     beq dec
 
     // converting the cases
-    cmp r5, #'A'
-    blt convert_case_r6
-    cmp r5, #'Z'
-    bgt convert_case_r6
-    add r5, r5, #32
+    cmp r5, #'A' // this checks if r5 is below 'A' (meaning it is not uppercase)
+    blt converting_r6
+    cmp r5, #'Z' // this checks if r5 is above 'Z' (meaning it is not uppercase)
+    bgt converting_r6
+    add r5, r5, #32 // if it is uppercase, converts r5 from uppercase to lowercase by adding 32
 
-convert_case_r6:
+converting_r6: // same as right before with r5
     cmp r6, #'A'
     blt comp
     cmp r6, #'Z'
@@ -71,8 +72,8 @@ comp:
     b   advance
 
 advance:
-    add r3, r3, #1          @ i++
-    sub r4, r4, #1          @ j--
+    add r3, r3, #1          // i++
+    sub r4, r4, #1          // j--
     b   check_palindrome
 
 inc:
@@ -84,21 +85,21 @@ dec:
     b   check_palindrome
 
 wild_left:
-    cmp r6, #32
-    beq not_palindrome            // was: beq not_palindrome
+    cmp r6, #32 // only checking if the corresponding character on the other side is a space, since it is a wildcard
+    beq not_palindrome // moving on if not space
     add r3, r3, #1
 	sub r4, r4, #1
     b   check_palindrome
 
 wild_right:
-    cmp r5, #32
-    beq not_palindrome            // was: beq not_palindrome
+    cmp r5, #32 
+    beq not_palindrome 
 	add r3, r3, #1
     sub r4, r4, #1
     b   check_palindrome
 
 is_palindrome:
-    pop {r4}  
+    pop {r4} // done to restore r4 and avoid clobbering
     ldr r1, =0xFF200000
 	mov r0, #0x1F
 	str r0, [r1]
@@ -106,7 +107,7 @@ is_palindrome:
     b write_jtag
 
 not_palindrome:
-    pop {r4} 
+    pop {r4} // done to restore r4 and avoid clobbering
     ldr r1, =0xFF200000
 	mov r0, #0x3E0
 	str r0, [r1]
@@ -115,10 +116,10 @@ not_palindrome:
 	
 write_jtag:
 	ldr r1, =0xFF201000
-	ldrb r2, [r0], #1
+	ldrb r2, [r0], #1 // loading the character from r0 and incrementing index
 	cmp r2, #0
 	beq _exit
-	str r2, [r1]
+	str r2, [r1] // writing character to jtag address
 	b write_jtag
 
 _exit:
